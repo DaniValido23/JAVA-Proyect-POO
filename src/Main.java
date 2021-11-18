e494661288efa2df0215c958526381708cb133b4
DaniValido23 <jvalidojeronimo@gmail.com>
2021-11-17 20:53:58 -0600

    Subiendo Proyecto a Github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int x=1;
         System.out.println("Subiendo Proyecto");
+        while(x<=10){
+            System.out.println("Soy "+x);
+            x++;
+        }
     }
 }
