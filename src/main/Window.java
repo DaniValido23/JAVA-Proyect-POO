4933fdd220096b49e18e71a9287ea563f1c2ca32
jorge251100 <jorge.aragonhe@anahuac.mx>
2021-11-20 23:31:44 -0600

    Videi 1 commpletado Notas: Cambie el nombre del juego  anexe esta clase que es Window a un Package

---
+++ src/main/Window.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+
+    public static final int WIDTH = 800, HEIGHT = 600;
+
+    public Window(){
+        setTitle("Defensores supremos");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Window();
+    }
+}
